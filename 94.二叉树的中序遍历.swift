d7c79327ea967b73a8018c47bf94932d595530ed
iCyrilh <cyr@meitu.com>
2021-04-12 05:23:46 +0800

    【Feature】第二周作业：[94-二叉树的中序遍历]-[https://leetcode-cn.com/problems/binary-tree-inorder-traversal/]

---
+++ 94.二叉树的中序遍历.swift
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=swift
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func inorderTraversal(_ root: TreeNode?) -> [Int] {
+        guard let r = root else { return [] }
+        var tree: [Int] = []
+        
+        tree += inorderTraversal(r.left)
+        tree.append(r.val)
+        tree += inorderTraversal(r.right)
+        
+        return tree
+    }
+}
+// @lc code=end
+
